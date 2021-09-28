979ec4d8c974444793495e2b1b4a2c61c43ac59c
Kirilll <vilyam13@gmail.com>
2021-09-28 21:25:53 +0300

    game field structure

--- script.js
+++ script.js
@@ -25,81 +25,79 @@ const gameState = {
 };
 
 const loginScreen = {
-  templateStructure: {
-    block: 'header',
-    cls: 'header',
-    content: [
-      {
-        block: 'div',
-        cls: 'container',
-        content: [
-          {
-            block: 'div',
-            cls: 'main',
-            content: [
-              {
-                block: 'h1',
-                cls: 'title',
-                content: [
-                  {
-                    block: 'span',
-                    cls: 'rock-text',
-                    innerText: 'Камень',
+  block: 'header',
+  cls: 'header',
+  content: [
+    {
+      block: 'div',
+      cls: 'container',
+      content: [
+        {
+          block: 'div',
+          cls: 'main',
+          content: [
+            {
+              block: 'h1',
+              cls: 'title',
+              content: [
+                {
+                  block: 'span',
+                  cls: 'rock-text',
+                  innerText: 'Камень',
+                },
+                {
+                  block: 'span',
+                  cls: 'scissors-text',
+                  innerText: 'Ножницы',
+                },
+                {
+                  block: 'span',
+                  cls: 'paper-text',
+                  innerText: 'Бумага',
+                },
+              ],
+            },
+            {
+              block: 'figure',
+              cls: 'title-image',
+              content: [
+                {
+                  block: 'div',
+                  cls: 'rock-image',
+                },
+                {
+                  block: 'div',
+                  cls: 'scissors-image',
+                },
+                {
+                  block: 'div',
+                  cls: 'paper-image',
+                },
+              ],
+            },
+            {
+              block: 'figure',
+              cls: 'login',
+              content: [
+                {
+                  block: 'input',
+                  cls: ['login', 'login-input'],
+                  attrs: {
+                    placeholder: 'Введи свой nikname',
                   },
-                  {
-                    block: 'span',
-                    cls: 'scissors-text',
-                    innerText: 'Ножницы',
-                  },
-                  {
-                    block: 'span',
-                    cls: 'paper-text',
-                    innerText: 'Бумага',
-                  },
-                ],
-              },
-              {
-                block: 'figure',
-                cls: 'title-image',
-                content: [
-                  {
-                    block: 'div',
-                    cls: 'rock-image',
-                  },
-                  {
-                    block: 'div',
-                    cls: 'scissors-image',
-                  },
-                  {
-                    block: 'div',
-                    cls: 'paper-image',
-                  },
-                ],
-              },
-              {
-                block: 'figure',
-                cls: 'login',
-                content: [
-                  {
-                    block: 'input',
-                    cls: ['login', 'login-input'],
-                    attrs: {
-                      placeholder: 'Введи свой nikname',
-                    },
-                  },
-                  {
-                    block: 'button',
-                    cls: ['login', 'login-button'],
-                    innerText: 'Войти',
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
+                },
+                {
+                  block: 'button',
+                  cls: ['login', 'login-button'],
+                  innerText: 'Войти',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
 
 const gameField = {
