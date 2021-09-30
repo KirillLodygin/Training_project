8fc4614b49c4c12f923d6af3618aed6d3014338f
Vakulenkoav17 <artutrs@gmail.com>
2021-09-30 23:43:44 +0700

    Add call to backend

--- standby_screen/standby_screen.js
+++ standby_screen/standby_screen.js
@@ -90,39 +90,220 @@ const standbyScreen = {
               },
             ],
           },
-          {
-            block: 'div',
-            cls: 'back_button_box',
-            method: {
-              eventName: 'click',
-              methodFunc: () => {
-                  clickButton();
-                  },
-              },
-            content: [
-              {
-                block: 'button',
-                cls: 'back_button_box-button',
-                content:[{
-                  
-                    block: 'img',
-                    cls: 'arrow',
-                    attrs: {
-                      src: './assets/img/back_arrow.png',
-                    },
-                  },
-                  {
-                    block: 'h3',
-                    innerText: 'Вернуться в лобби',
-                  },          
-                ],
-              },
-            ],
-          },
+          
+          
         ],
       },
     ],
   
+};
+const backToLobbyButton ={
+  block: 'div',
+  cls: 'back_button_box',
+  method: {
+    eventName: 'click',
+    methodFunc: () => {
+      backToLobby();
+        },
+    },
+  content: [
+    {
+      block: 'button',
+      cls: 'back_button_box-button',
+      content:[{
+        
+          block: 'img',
+          cls: 'arrow',
+          attrs: {
+            src: './assets/img/back_arrow.png',
+          },
+        },
+        {
+          block: 'h3',
+          innerText: 'Вернуться в лобби',
+        },          
+      ],
+    },
+  ],
+};
+
+
+const lobby =  {
+  block: 'div',
+  cls: 'wrapper',
+  content: [
+      {
+          block: 'div',
+          cls: 'header',
+          content: [
+              {
+                  block: 'h1',
+                  innerText: 'Добро пожаловать в Лобби',
+              },
+          ],
+      },
+      {
+          block: 'div',
+          cls: 'columnHeaders',
+          content: [
+              {
+                  block: 'div',
+                  cls: 'columnHeader',
+                  content: [
+                      {
+                          block: 'h2',
+                          innerText: 'Вы',
+                      },
+                  ],
+              },
+              {
+                  block: 'div',
+                  cls: 'columnHeader2',
+                  content: [
+                      {
+                          block: 'h2',
+                          innerText: 'Доступные игры',
+                      },
+                  ],
+              },
+          ],
+      },
+      {
+          block: 'div',
+          cls: 'players',
+          content: [
+              {
+                  block: 'div',
+                  cls: 'main_yourself_profile-block',
+                  content: [
+                      {
+                          block: 'div',
+                          cls: 'main_yourself_profile-block-header',
+                          content: [
+                              {
+                                  block: 'img',
+                                  cls: 'yourself_profile-avatar',
+                                  attrs: {
+                                      src: 'assets/img/avatar.png',
+                                  },
+                              },
+                              {
+                                  block: 'h2',
+                                  cls: 'yourself_profile-name',
+                                  innerText: 'UserName',
+                              },
+                          ],
+                      },
+                      {
+                          block: 'div',
+                          cls: 'yourself_profile_statistics-block',
+                          content: [
+                              {
+                                  block: 'h3',
+                                  cls: 'statistics-header',
+                                  innerText: 'Статистика',
+                              },
+                              {
+                                  block: 'div',
+                                  cls: 'statistic-items',
+                                  content: [
+                                      {
+                                          block: 'p',
+                                          cls: 'win',
+                                          innerText: 'Победы : 0',
+                                      },
+                                      {
+                                          block: 'p',
+                                          cls: 'loose',
+                                          innerText: 'Поражения : 0 ',
+                                      },
+                                      {
+                                          block: 'p',
+                                          cls: 'draw',
+                                          innerText: 'Ничьи : 0',
+                                      },
+                                  ],
+                              },
+                          ],
+                      },
+                  ],
+              },
+              {
+                  block: 'div',
+                  cls: 'opponents',
+                  content: [
+                      {
+                          block: 'div',
+                          cls: 'main_opponent_profile-block',
+                          content: [
+                              {
+                                  block: 'div',
+                                  cls: 'main_opponent_profile-block-header',
+                                  content: [
+                                      {
+                                          block: 'img',
+                                          cls: 'opponent_profile-avatar',
+                                          attrs: {
+                                              src: 'assets/img/avatar.png',
+                                          },
+                                      },
+                                      {
+                                          block: 'h3',
+                                          cls: 'opponent_profile-name',
+                                          innerText: 'Пётр',
+                                      },
+                                  ],
+                              },
+                              {
+                                  block: 'div',
+                                  cls: 'opponent_profile_statistics-block',
+                                  content: [
+                                      {
+                                          block: 'h3',
+                                          cls: 'statistics-header',
+                                          innerText: 'Статистика Противника',
+                                      },
+                                      {
+                                          block: 'div',
+                                          cls: 'statistic-items',
+                                          content: [
+                                              {
+                                                  block: 'p',
+                                                  cls: 'win',
+                                                  innerText: 'Победы : 0',
+                                              },
+                                              {
+                                                  block: 'p',
+                                                  cls: 'loose',
+                                                  innerText: 'Поражения : 0',
+                                              },
+                                              {
+                                                  block: 'p',
+                                                  cls: 'draw',
+                                                  innerText: 'Ничьи : 0',
+                                              },
+                                          ],
+                                      },
+                                  ],
+                              },
+                              {
+                                  block: 'div',
+                                  cls: 'enterBlock',
+                                  content: [
+                                      {
+                                          block: 'button',
+                                          cls: 'enterButton',
+                                          innerText: 'Войти в игру',
+                                      },
+                                  ],
+                              },
+                          ],
+                      },
+                  ],
+              },
+          ],
+      },
+  ],
 };
 const app = document.querySelector(".app");
 const templateEngine = block => {
@@ -184,10 +365,65 @@ const createScreen = (obj) => {
 const createBlock = (arrObj, parentNode) => {
 	parentNode.appendChild(templateEngine(arrObj));
 };
-
+const gameStatus = "http://localhost:3000/game-status"
+ 
+function createPageStandByScreen() {
 createScreen(standbyScreen);
-createBlock(standbyScreen.block, document.querySelector('.back_button_box'));
+createBlock(backToLobbyButton, document.querySelector(".undercard"))
 
-function clickButton() {
-	console.log('click');
+let timer = setInterval(checkOpponentConnection, 2000);
+
+};
+
+function test (data){
+  alert(data)
 }
+//переход на экран лобби
+
+function backToLobby() {
+createScreen(lobby)  
+clearTimeout  (timer)
+}
+//переход на экран игры
+function toGame(){
+  createScreen(gameScreen)
+}
+
+
+function request(url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url );
+  xhr.addEventListener('readystatechange', function (e) {
+      if (e.target.readyState !== 4) {
+          return;
+      }
+      if (e.target.status !== 200) {
+          console.log('Ошибка');
+          return;
+      }
+      const responseText = e.target.responseText;
+      callback(responseText);
+  });
+  xhr.send();
+}
+//Будет вызывать get player list и парсить его
+function getUserInfo(){}
+
+//Запрос данных с сервера
+ function checkOpponentConnection(){
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', "http://localhost:3000/game-status" );
+  xhr.addEventListener('readystatechange', function (e) {
+      if (e.target.readyState !== 4) {
+          return;
+      }
+      if (e.target.status !== 200) {
+          console.log('Ошибка');
+          return;
+      }
+      const responseText = e.target.responseText;
+      test(responseText);
+  });
+  xhr.send();
+}
+document.addEventListener("DOMContentLoaded", createPageStandByScreen)
