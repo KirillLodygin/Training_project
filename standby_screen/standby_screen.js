7c667d26bf398800409e0527bc2889fd9f84d149
Vakulenkoav17 <artutrs@gmail.com>
2021-09-30 13:28:18 +0700

    addeded functions

--- standby_screen/standby_screen.js
+++ standby_screen/standby_screen.js
@@ -1,5 +1,5 @@
 const standbyScreen = {
-  templateStructure: {
+  
     block: 'div',
     cls: 'container',
     content: [
@@ -93,6 +93,12 @@ const standbyScreen = {
           {
             block: 'div',
             cls: 'back_button_box',
+            method: {
+              eventName: 'click',
+              methodFunc: () => {
+                  clickButton();
+                  },
+              },
             content: [
               {
                 block: 'button',
@@ -116,5 +122,72 @@ const standbyScreen = {
         ],
       },
     ],
-  },
+  
 };
+const app = document.querySelector(".app");
+const templateEngine = block => {
+  if (!block) {
+    return document.createTextNode('');
+  }
+
+  if (
+    typeof block === 'string' ||
+    typeof block === 'number' ||
+    block === true
+  ) {
+    return document.createTextNode(String(block));
+  }
+
+  if (Array.isArray(block)) {
+    const fragment = document.createDocumentFragment();
+
+    block.forEach(contentItem => {
+      const el = templateEngine(contentItem);
+
+      fragment.appendChild(el);
+    });
+
+    return fragment;
+  }
+
+  const element = document.createElement(block.block);
+
+  []
+    .concat(block.cls)
+    .filter(Boolean)
+    .forEach(className => element.classList.add(className));
+
+  if (block.attrs) {
+    Object.keys(block.attrs).forEach(key => {
+      element.setAttribute(key, block.attrs[key]);
+    });
+  }
+
+  if (block.innerText) element.innerText = block.innerText;
+
+  if (block.method) element.addEventListener(block.method.eventName, block.method.methodFunc);
+
+  element.appendChild(templateEngine(block.content));
+
+  return element;
+};
+
+
+const createScreen = (obj) => {
+  while (app.firstChild) {
+    app.firstChild.remove();
+  }
+
+  app.appendChild(templateEngine(obj));
+};
+
+const createBlock = (arrObj, parentNode) => {
+	parentNode.appendChild(templateEngine(arrObj));
+};
+
+createScreen(standbyScreen);
+createBlock(standbyScreen.block, document.querySelector('.back_button_box'));
+
+function clickButton() {
+	console.log('click');
+}
