c0766740b1a5cb93f9c132f43846ca2deaba14d5
Sheldin1993 <86757615+Sheldin1993@users.noreply.github.com>
2021-09-30 22:35:41 +0700

    Add files via upload

---
+++ staticScreenMarkUp.js
@@ -0,0 +1,48 @@
+﻿// JavaScript source code
+const lobbyScreen = {
+    block: 'div',
+    cls: 'wrapper',
+    content: [{
+        block: 'div',
+        cls: 'header',
+        content: {
+            block: 'h1',
+            innerText: 'Добро пожаловать в лобби'
+        }
+    },
+    {
+        block: 'div',
+        cls: 'columnHeaders',
+        content: [{
+            block: 'div',
+            cls: 'columnHeader',
+            content: [{
+                block: 'h2',
+                innerText: 'Вы'
+            },
+            {
+                block: 'div',
+                cls: 'columnHeader2',
+                content: {
+                    block: 'h2',
+                    innerText: 'Доступные игры'
+                },
+
+            },
+            {
+                block: 'div',
+                cls: 'popUpWaitingScreen',
+                content: [{
+                    block: 'p',
+                    cls: 'loadingStatus',
+                    innertext: 'Получение данных от сервера. Подождите'
+                },
+                {
+                    block: 'div',
+                    cls: 'loader',
+                    innerText: 'Loading...'
+                }
+                ]
+            }
+            ]
+        }
